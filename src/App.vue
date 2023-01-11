be063a1cd891285eb0e960e6f540576b28d2f74b
jagadeeswara_reddy <jagadeeswara.reddy@otocapital.in>
2023-01-12 01:37:22 +0530

    base commit it;s working now have to work on the styling part :/

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <p>To show the viewer, you have to click on the images.</p>
+    <div v-viewer="options" class="images clearfix">
+      <img
+        v-for="{ source, thumbnail } in images"
+        :key="source"
+        :src="thumbnail"
+        :data-source="source"
+        class="image"
+        :alt="source.split('?image=').pop()"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { directive as viewer } from "v-viewer";
+
+const sourceImages = [];
+const base = Math.floor(Math.random() * 60) + 10;
+for (let i = 0; i < 10; i++) {
+  sourceImages.push({
+    thumbnail: `https://picsum.photos/id/${base + i}/346/216`,
+    source: `https://picsum.photos/id/${base + i}/1440/900`,
+  });
+}
+
+export default {
+  directives: {
+    viewer: viewer({
+      // debug: true,
+    }),
+  },
+  data() {
+    return {
+      options: {
+        toolbar: true,
+        url: "data-source",
+      },
+      images: sourceImages,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.image {
+  width: calc(30% - 20px);
+  cursor: pointer;
+  margin: 5px;
+  display: inline-block;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+import VueViewer from 'v-viewer';
+
+import "viewerjs/dist/viewer.css"
+
+import './assets/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.use(VueViewer)
+
+app.mount('#app')
